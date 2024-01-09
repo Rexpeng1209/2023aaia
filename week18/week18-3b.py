729c00239c3c61667ce82909b80773c48938751f
rexpeng1209 <min931209@gmail.com>
2024-01-09 12:03:12 +0800

    week18

---
+++ week18/week18-3b.py
@@ -0,0 +1,4 @@
+a=input('請輸入文字:')
+b=input('請輸入第二個數字:')
+ans=int(a)+int(b)
+print('答案是', ans)
---
+++ week18/week18-4a.py
@@ -0,0 +1,8 @@
+a=int('12345677')
+print(a+999)
+#bool() False True
+#int() 0 1 100 1999 -33
+#flout() 3.1415 2.7182
+#sru() '單引號' "雙引號"
+ans=type('12345')
+print(ans)
